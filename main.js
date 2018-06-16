e0a165d3b819ee750d797bc7b9946dd24f17cb53
Luisk <ldelvalleu@ucenfotec.ac.cr>
2018-06-15 23:51:14 -0600

    request reportes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.addEventListener('load', init, false);
 
 function init() {
 
-    var urlBase = 'https://gallopintoadmin.firebaseio.com/reportes.json';
+    var urlBase = 'https://gallopintocalladmin.firebaseio.com/reportes.json';
     var txtNombre = document.getElementById('txtNombre');
     var txtApellido = document.getElementById('txtApellido');
     var txtEmail = document.getElementById('txtEmail');
@@ -15,7 +15,38 @@ function init() {
     var autor = 'Luisk';
 
     btnGuardar.onclick = crearReporte;
-    mostrarReportes();
+    requestAllReportes();
+
+    function requestAllReportes() {
+        var request = new XMLHttpRequest();
+        request.open('GET', urlBase, true);
+        request.onreadystatechange = requestAllReportesCallBack;
+        request.send();
+    }
+
+    function requestAllReportesCallBack(event) {
+        var request = event.target;
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status == 200) {
+                reportes = [];
+                var reportesData = JSON.parse(request.responseText);
+                for (const key in reportesData) {
+                    var reporteData = reportesData[key];
+                    // var editable = false;
+                    // if (reporteData.owner === owner) {
+                    //     editable = true;
+                    // }
+                    // var date = new Date(reporteData.timestamp);
+                    var reporte = new Reporte(key, reporteData.nombre, reporteData.apellido, reporteData.telefono, reporteData.email, reporteData.cedula, reporteData.reporteLlamada, reporteData.fecha, reporteData.autor);
+                    reportes.push(reporte);
+                }
+                mostrarReportes();
+            } else {
+                console.log('Error on request: ', request.status);
+            }
+        }
+    }
+
 
     function crearReporte() {
         var reporte = new Reporte(null, txtNombre.value, txtApellido.value, txtEmail.value, txtTelefono.value, txtCedula.value, txtReporte.value, null, autor);
